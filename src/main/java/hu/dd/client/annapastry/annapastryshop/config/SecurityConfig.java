ebacebeeab9376181ec1f2b985d0d3986de2a996
Benedek Halasi <benhalasi@gmail.com>
2022-08-19 18:35:06 +0200

    security config - login option

--- src/main/java/hu/dd/client/annapastry/annapastryshop/config/SecurityConfig.java
+++ src/main/java/hu/dd/client/annapastry/annapastryshop/config/SecurityConfig.java
@@ -4,26 +4,33 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-  // @Bean
-	// public UserDetailsService userDetailsService() {
-	// 	InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-	// 	manager.createUser(User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build());
-	// 	return manager;
-	// }
-
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
       .authorizeRequests(authorize -> authorize
-        .anyRequest().permitAll()
-      );
+        .antMatchers("/annapastry/**").permitAll()
+        .antMatchers("/error").permitAll()
+        .anyRequest().authenticated()
+      )
+      .formLogin()
+      // .httpBasic()
+      ;
     return http.build();
   }
 
+  @Bean
+  public UserDetailsService userDetailsService() {
+    InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+    manager.createUser(User.withDefaultPasswordEncoder().username("user").password("aabbccdd").roles("USER").build());
+    return manager;
+  }
 }
