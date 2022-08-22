06d669ac531fdaf477c75f6f651e5b4bd67dce75
Benedek Halasi <benhalasi@gmail.com>
2022-08-22 19:23:42 +0200

    lighthouse improvements

--- src/main/java/hu/dd/client/annapastry/annapastryshop/demo/ControllerMain.java
+++ src/main/java/hu/dd/client/annapastry/annapastryshop/demo/ControllerMain.java
@@ -17,17 +17,17 @@ public class ControllerMain {
   @GetMapping({"", "/"})
   public String mainPage (ModelMap model) {
     model.addAttribute("jumbotron", servicePicture.main());
-    model.addAttribute("thumbnail", servicePicture.mainThumbnail());
+    model.addAttribute("thumbnail", servicePicture.mainThumbnail().setAlt("Pastry promo"));
     model.addAttribute("rows", List.of(
       List.of(
-        servicePicture.product(4),
-        servicePicture.product(5),
-        servicePicture.product(6)
+        servicePicture.product(4).setAlt("Specifikus Termék Kép").setTitle("Specifikus Termék Kép").setAspectRatio(5184d / 3456),
+        servicePicture.product(5).setAlt("Specifikus Termék Kép").setTitle("Specifikus Termék Kép").setAspectRatio(5184d / 3456),
+        servicePicture.product(6).setAlt("Specifikus Termék Kép").setTitle("Specifikus Termék Kép").setAspectRatio(4942d / 3396)
       ),
       List.of(
-        servicePicture.product(1),
-        servicePicture.product(2),
-        servicePicture.product(3)
+        servicePicture.product(1).setAlt("Specifikus Termék Kép").setTitle("Specifikus Termék Kép").setAspectRatio(3456d / 5184),
+        servicePicture.product(2).setAlt("Specifikus Termék Kép").setTitle("Specifikus Termék Kép").setAspectRatio(3456d / 5184),
+        servicePicture.product(3).setAlt("Specifikus Termék Kép").setTitle("Specifikus Termék Kép").setAspectRatio(3456d / 5184)
       )
     ));
     return Pages.main.getThymeReference();
@@ -36,14 +36,14 @@ public class ControllerMain {
   @GetMapping({"/product", "/product/{productId}"})
   public String productPage (ModelMap model) {
     model.addAttribute("pics", List.of(
-      servicePicture.product(1),
-      servicePicture.product(2),
-      servicePicture.product(4),
-      servicePicture.product(3),
-      servicePicture.product(5),
-      servicePicture.product(5),
-      servicePicture.product(6),
-      servicePicture.product(1)
+      servicePicture.product(1).setAlt("Specifikus Termék Kép").setTitle("Specifikus Termék Kép").setAspectRatio(3456d / 5184),
+      servicePicture.product(2).setAlt("Specifikus Termék Kép").setTitle("Specifikus Termék Kép").setAspectRatio(3456d / 5184),
+      servicePicture.product(4).setAlt("Specifikus Termék Kép").setTitle("Specifikus Termék Kép").setAspectRatio(5184d / 3456),
+      servicePicture.product(3).setAlt("Specifikus Termék Kép").setTitle("Specifikus Termék Kép").setAspectRatio(3456d / 5184),
+      servicePicture.product(5).setAlt("Specifikus Termék Kép").setTitle("Specifikus Termék Kép").setAspectRatio(5184d / 3456),
+      servicePicture.product(5).setAlt("Specifikus Termék Kép").setTitle("Specifikus Termék Kép").setAspectRatio(5184d / 3456),
+      servicePicture.product(6).setAlt("Specifikus Termék Kép").setTitle("Specifikus Termék Kép").setAspectRatio(4942d / 3396),
+      servicePicture.product(1).setAlt("Specifikus Termék Kép").setTitle("Specifikus Termék Kép").setAspectRatio(3456d / 5184)
 
     ));
     return Pages.product.getThymeReference();
@@ -51,8 +51,8 @@ public class ControllerMain {
 
   @GetMapping({"/about"})
   public String aboutPage (ModelMap model) {
-    model.addAttribute("p1", servicePicture.about(1));
-    model.addAttribute("p2", servicePicture.about(2));
+    model.addAttribute("p1", servicePicture.about(1).setAlt("Rólunk kép 1").setTitle("Rólunk kép 1"));
+    model.addAttribute("p2", servicePicture.about(2).setAlt("Rólunk kép 2").setTitle("Rólunk kép 2"));
     return Pages.about.getThymeReference();
   }
 }
